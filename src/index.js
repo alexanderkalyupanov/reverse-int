572e943c8746c0736a239e5625dfc980bba8b1f9
Alexander Kalyupanov <alexander.kalyupanov@mail.ru>
2024-07-23 16:54:06 +0300

    feat: add added a solution without considering -

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 module.exports = function reverse (n) {
-  
+    n = n.toString()
+    let revNumber = "";
+    for (let i = n.length - 1; i >= 0; i--) {
+        revNumber += n[i]
+    }
+    return revNumber;
 }
